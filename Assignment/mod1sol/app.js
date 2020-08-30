b91da22acb87ca45c9d6844bc258fc2c5e63fcf5
Nidhi132 <nidhi.saale@gmail.com>
2020-08-30 17:15:54 +0530

    Assignment 1

--- Assignment/mod1sol/app.js
+++ Assignment/mod1sol/app.js
@@ -1,24 +1,56 @@
 (function () {
-'use strict';
+  'use strict';
 
-angular.module('LunchCheck', [])
+  angular.module('LunchCheck',[]).controller('LunchCheckController', LunchCheckController);
 
-.controller('LunchCheckController', function ($scope) {
-  $scope.name = "";
-  $scope.msg = "";
-  LunchCheckController.$inject = ['$scope'];
-  
+  LunchCheckController.$inject =['$scope'];
 
-  $scope.CalculateItem = function (string) {
-    var words = string.split(',');
-    console.log(words);
-    if(words.length<=3)
-      msg = "ENJOY!";
-    else
-      msg = "Too Much";
-  };
-
-});
-
-
+  function LunchCheckController ($scope){
+    $scope.list = '';
+    $scope.msg = '';
+    $scope.fontStyle = {};
+    $scope.boxStyle = {};
+    
+    $scope.CalculateItems = function () {
+      console.log ('The Items');
+      console.log ($scope.list);
+      if($scope.list !== '') {
+        var arr = $scope.list.split(',');
+        var count =0;
+        for(var i =0; i<arr.length; i++){
+          if(arr[i].trim() !=='')
+            count++;
+        }
+        if(count > 0){
+          if(count <=3){
+            $scope.msg = 'ENJOY!';
+          }
+          else if(count > 3){
+            $scope.msg = 'Too Much!';
+          }
+          $scope.fontStyle = {
+            "color":"green"
+          };
+          $scope.boxStyle = {
+            "border-color":"green"
+          };
+        }
+        else {
+          enterItems();
+        }
+      }
+      else {
+        enterItems();
+      }
+    }
+    var enterItems = function() {
+      $scope.msg = 'Please enter data first';
+        $scope.fontStyle = {
+          "color":"red"
+        };
+        $scope.boxStyle = {
+          "border-color":"red"
+        };
+    };
+  }
 })();
